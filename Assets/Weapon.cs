fef7b7020b3ea8c64c5e57a3f39abcb7c84d507e
Finn Pauls <mbp@pauls.dev>
2022-11-21 17:36:39 +0100

    Weapon attacks when enemy is in range

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -2,21 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum WeaponState
+{
+    Idle,
+    Firing,
+    Returning
+}
+
 public class Weapon : MonoBehaviour
 {
+    public float coolDown;
+    public float damage;
+    public float range;
+    public float speed;
+
+    WeaponState state = WeaponState.Idle;
+
+    float remainingCoolDown = 0f;
+
+    float firingAngle;
+    Vector3 startPoint;
+
     void Update()
     {
-        var closestEnemy = FindClosestEnemy();
+        if (state == WeaponState.Idle)
+            HandleIdle();
 
-        if (closestEnemy != null)
-        {
-            var direction = closestEnemy.transform.position - transform.position;
-            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+        if (state == WeaponState.Firing)
+            HandleFiring();
+
+        if (state == WeaponState.Returning)
+            HandleReturning();
     }
 
-    GameObject FindClosestEnemy()
+    void HandleIdle()
+    {
+        var (closestEnemy, enemyDistance) = FindClosestEnemy();
+
+        if (closestEnemy == null)
+            return;
+
+        var angle = PointTowardsEnemy(closestEnemy);
+
+        if (enemyDistance <= range && remainingCoolDown <= 0f)
+        {
+            Fire(angle);
+            remainingCoolDown = coolDown;
+        }
+
+        if (remainingCoolDown > 0f)
+            remainingCoolDown -= Time.deltaTime;
+    }
+
+    void HandleFiring()
+    {
+        transform.position +=
+            new Vector3(
+                Mathf.Cos(firingAngle * Mathf.Deg2Rad),
+                Mathf.Sin(firingAngle * Mathf.Deg2Rad),
+                0
+            )
+            * speed
+            * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, startPoint) >= range)
+            state = WeaponState.Returning;
+    }
+
+    void HandleReturning()
+    {
+        transform.localPosition = Vector3.MoveTowards(
+            transform.localPosition,
+            new Vector3(0, 0, 0),
+            speed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.localPosition, new Vector3(0, 0, 0)) <= 0.01f)
+            state = WeaponState.Idle;
+    }
+
+    void Fire(float angle)
+    {
+        state = WeaponState.Firing;
+        startPoint = transform.position;
+        firingAngle = angle;
+    }
+
+    float PointTowardsEnemy(GameObject enemy)
+    {
+        var direction = enemy.transform.position - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+        return angle;
+    }
+
+    (GameObject, float) FindClosestEnemy()
     {
         float closestDistance = float.MaxValue;
         foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
@@ -25,10 +106,10 @@ public class Weapon : MonoBehaviour
             if (distance < closestDistance)
             {
                 closestDistance = distance;
-                return enemy;
+                return (enemy, distance);
             }
         }
 
-        return null;
+        return (null, 0f);
     }
 }
